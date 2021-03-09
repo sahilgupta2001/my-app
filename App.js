e0e0ee411f149602de6069db054df1b67a6ca0f8
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2021-03-09 14:43:57 -0800

    Added the files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My new React native app</Text>
+      <Text>My new React native app code</Text>
       <StatusBar style="auto" />
     </View>
   );
